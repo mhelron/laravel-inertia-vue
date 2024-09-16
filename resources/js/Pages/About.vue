3e5aac8bbf1b99fa21626b72e694e201ffe6f830
ronmhel <salcedo.131437@marikina.sti.edu.ph>
2024-09-16 16:54:05 +0800

    Learning Routes

---
+++ resources/js/Pages/About.vue
@@ -0,0 +1,11 @@
+<script setup>
+    defineProps({
+        user: String
+    })
+</script>
+
+<template>
+    <div>
+        <h1>This is the about page, {{ user }}</h1>
+    </div>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,6 @@ Use Inertia\Inertia;
 Route::get('/', function () {
     return inertia::render('Home');
 });
+
+//              URL, COMPONENT, DATA
+Route::inertia('/about', 'About', ['user' => 'Mhel']);
