e4110d2cd9a2e6ed89e46244967a8feab70edc06
ronmhel <salcedo.131437@marikina.sti.edu.ph>
2024-09-16 19:57:24 +0800

    Learning Text Input Component

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -1,8 +1,7 @@
 <script setup>
 import { useForm } from '@inertiajs/vue3';
-import { reactive } from 'vue';
 import { route } from '../../../../vendor/tightenco/ziggy/src/js';
-
+import TextInput from '../Components/TextInput.vue';
 
 const form = useForm({
     name: null,
@@ -27,28 +26,10 @@ const submit = () => {
     <div class="w=2/4 mx-auto">
         <form @submit.prevent="submit">
 
-            <div class="mb-6">
-                <label>Name</label>
-                <input type="text" v-model="form.name" />
-                <small>{{ form.errors.name }}</small>
-            </div>
-
-            <div class="mb-6">
-                <label>Email</label>
-                <input type="text" v-model="form.email"/>
-                <small>{{ form.errors.email }}</small>
-            </div>
-
-            <div class="mb-6">
-                <label>Password</label>
-                <input type="password" v-model="form.password"/>
-                <small>{{ form.errors.password }}</small>
-            </div>
-
-            <div class="mb-6">
-                <label>Confirm Password</label>
-                <input type="password" v-model="form.password_confirmation"/>
-            </div>
+            <TextInput name="name" v-model="form.name" :message="form.errors.name"/>
+            <TextInput name="email" type="email" v-model="form.email" :message="form.errors.email"/>
+            <TextInput name="password" type="password" v-model="form.password" :message="form.errors.password"/>
+            <TextInput name="confirm password" type="password" v-model="form.password_confirmation" :message="form.errors.password_confirmation"/>
 
             <div>
                 <p class="text-slate-600 mb-2">Already a user? <a href="#" class="text-link">Login</a></p>
---
+++ resources/js/Pages/Components/TextInput.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+const model = defineModel({
+    type: null,
+    required: true,
+})
+    defineProps({
+        name: {
+            type: String,
+            Required: true,
+        },
+        type: {
+            type: String,
+            default: 'text',
+        },
+        message: String
+    })
+</script>
+
+<template>
+    <div class="mb-6">
+        <label>{{ name }}</label>
+        <input :type="type" v-model="model" :class="{'!ring-red-500' : message}"/>
+        <small style="color: red;" v-if="message">{{ message }}</small>
+    </div>
+</template>
