4f03263bf0e0ef2ff2871da64217924a1b6e4688
ronmhel <salcedo.131437@marikina.sti.edu.ph>
2024-09-16 19:09:54 +0800

    Learning Scroll Preserving

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -7,4 +7,6 @@
     <div>
         <h1>This is the home page, {{ $page.props.auth.user }}</h1>
     </div>
+
+    <Link class="mt-[1400px] block" href="/" preserve-scroll>Refresh</Link> 
 </template>
